920192e941d9a94410c081987ebac687b9b9e56a
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2026-09-20 16:50:47 +0600

    first commit

---
+++ src/apis/auth/table.ts
@@ -0,0 +1,9 @@
+import { pool } from "../../db/connectDB.ts";
+
+const authTable = async ()=>{
+    console.log("creating user table");
+    const query = `
+    CREATE 
+    `
+    // await pool.query(query);
+}
