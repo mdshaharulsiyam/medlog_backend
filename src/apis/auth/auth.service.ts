ba001d45d9c3a98fbb7ebd0ca0bfba19370ee0ae
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2026-09-21 16:52:30 +0600

    otp module created

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -8,32 +8,28 @@ const SignUp = async (body: SignUpData) => {
 
   const { email, password, username } = body;
 
-  const getExistingQuery = `
-    SELECT * FROM users WHERE email = $1 LIMIT 1
-    `;
+  const getExistingQuery = `SELECT * FROM users WHERE email = $1 LIMIT 1`;
 
   const existingUser = await pool.query(getExistingQuery, [email]);
+
   if (existingUser?.rowCount == 0) {
     
-        const insertQuery = `
-            INSERT INTO users (username,email,password,role)
-            VALUES ($1,$2,$3,$4)`;
+        const insertQuery = `INSERT INTO users (username,email,password,role) VALUES ($1,$2,$3,$4)`;
 
-        const createUser = await pool.query(insertQuery, [
+        await pool.query(insertQuery, [
           username,
           email,
           await hashText(password),
           "user",
         ]);
-        if (createUser) {
-            const otp = await otpService.create({ email }); 
+
+        await otpService.create({ email }); 
           return {
             success: true,
             message: "account created successfully",
             note: "please verify your account before login",
-            otp,
           };
-        }
+        
   }
   await otpService.create({ email }); 
   return {
